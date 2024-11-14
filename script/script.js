18c5d59f11477f9e1e1340288ba5260fd819c4cd
LuigiRac <lui.rac06@gmail.com>
2024-11-14 01:36:56 +0100

    console.log(username.value);

--- script/script.js
+++ script/script.js
@@ -4,3 +4,21 @@ console.clear();
 
 
 const username = document.getElementById("username");
+const kilometers = document.getElementById("kilometers");
+const userAge = document.getElementById("userAge");
+const genera = document.getElementById("genera");
+const annulla = document.getElementById("annulla");
+
+
+const nomePasseggero = document.getElementById("nome-passegero");
+const offerta = document.getElementById("offerta");
+const carrozza = document.getElementById("carrozza");
+const codiceCp = document.getElementById("codice-cp");
+const costoBiglietto = document.getElementById("costo-biglietto");
+
+
+genera.addEventListener("click", function () {
+    nomePasseggero.innerHTML = username.value;
+    // console.log(username.value);
+});
+
