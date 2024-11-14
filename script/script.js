5dffd032c71a7cbabe0fc24d3c4e62ad595303fe
LuigiRac <lui.rac06@gmail.com>
2024-11-14 02:10:37 +0100

    ticketPrice + if(discountTicket)

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 "use strict"
 console.clear();
 
-
+const priceKm = 0.21;
+const discountJunior = 0.2;
+const discountSenior = 0.4;
 
 const username = document.getElementById("username");
 const kilometers = document.getElementById("kilometers");
@@ -16,9 +18,23 @@ const carrozza = document.getElementById("carrozza");
 const codiceCp = document.getElementById("codice-cp");
 const costoBiglietto = document.getElementById("costo-biglietto");
 
+let discountTicket = 0;
 
 genera.addEventListener("click", function () {
     nomePasseggero.innerHTML = username.value;
     // console.log(username.value);
+    const valueKm = parseFloat(kilometers.value);
+
+    const userAgeValue = userAge.value;
+    const ticketPrice = priceKm * valueKm;
+    // console.log(ticketPrice);
+    if (userAgeValue == "minorenne") {
+        discountTicket = ticketPrice * discountJunior;
+    } else if (userAgeValue == "maggiorenne") {
+        discountTicket = 0;
+    } else if (userAgeValue == "over") {
+        discountTicket = ticketPrice * discountSenior;
+    }
 });
+
 
